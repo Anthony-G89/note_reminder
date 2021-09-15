2485dffe1e0b3286cdb00de58c92484408687c4e
Anthony Guerrero <adguerrero1935@gmail.com>
2021-09-15 18:00:45 -0400

    Adding folder structure and deleted files that I don't need

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div >
+      <h1>Hello MARIO?</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ schema.sql
@@ -0,0 +1,3 @@
+/* Create and use the note_Reminder db */
+DROP DATABASE IF EXISTS 'note_reminder';
+CREATE DATABASE 'note_reminder';
