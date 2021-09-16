1496dadbe2758b49512db699483d63178d94d57e
Anthony Guerrero <adguerrero1935@gmail.com>
2021-09-16 18:51:58 -0400

    Just added a text-align to my text

--- client/src/components/Landing-page-Decription/index.js
+++ client/src/components/Landing-page-Decription/index.js
@@ -9,10 +9,10 @@ const Description = () => {
         <div className="descriptionWrapper">
             <div className="descriptionContainer">
                 <h2 className="descriptionTitle"> Welcome to Note Reminder!</h2>
-                <p className="decriptionText">If you're someone that have a lot of things going on your life and sometimes forget certain things, will this application is for you. Note Reminder will let you create, read, update and delete a note. All you have to do is register and start adding your notes</p>
+                <p className="decriptionText">If you're someone that have a lot of things going on your life and sometimes forget certain things, will this application is for you. Note Reminder will let you create, read, update and delete a note. All you have to do is register and start adding your notes.</p>
             </div>
             <div className="linksContainer">
-            <a className="registerLink links" href="#">Register</a>
+                <a className="registerLink links" href="#">Register</a>
                 <a className="loginLink links" href="#">Log In</a>
             </div>
         </div>
