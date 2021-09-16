63ec1ceefb70ca9f2924a5b69e1cc847767b0ec4
Anthony Guerrero <adguerrero1935@gmail.com>
2021-09-15 21:12:54 -0400

    Added a video and now trying to get it to autoplay

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-
+/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 function App() {
   return (
     <div >
-      <h1>Hello MARIO?</h1>
+      {/* <img src={process.env.PUBLIC_URL + "./assets/login-img.jpg"} width={200} height={200}/> */}
+      <video width="95%" height="90%" controls autoplay muted id="backGroundVideo" >
+        <source src={process.env.PUBLIC_URL + "./assets/Note-Reminder-Background-Video.mp4"} type="video/mp4"/>
+      </video>
     </div>
   );
 }
