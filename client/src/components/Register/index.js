613225ec6110664b806aadc487e43bbba0024331
Anthony Guerrero <adguerrero1935@gmail.com>
2021-09-21 18:52:53 -0400

    Finished on the register page with structure and design. Will come back if I need to make a change

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,10 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import "./style.css";
+import { Link } from 'react-router-dom';
 
 
 
 const Register = () => {
+
+function registerUser  ()  {
+    alert("Register btn is working")
+}
+
     return (
         <div className="registerWrapper">
             <div className="registerContainer row align-items-center">
@@ -12,17 +19,24 @@ const Register = () => {
                     <img src={process.env.PUBLIC_URL + "./assets/register-img-min.jpg"} className="registerImage" alt="Image for register page" />
                 </div>
                 <div className="col-6">
-                    <form>
-                        <label>First Name</label>
-                        <input />
-                        <label>Last Name</label>
-                        <input />
-                        <label>Email</label>
-                        <input />
-                        <label>Password</label>
-                        <input />
+                    <form className="formWrapper">
+                        <label className="registerLabel">First Name</label>
+                        <input className="registerInput" />
+                        <label className="registerLabel">Last Name</label>
+                        <input className="registerInput" />
+                        <label className="registerLabel">Email</label>
+                        <input className="registerInput" />
+                        <label className="registerLabel">Password</label>
+                        <input className="registerInput" />
 
+                        <p className="alreadyAMember">Already on Note Reminder?
+                            <Link><a className="loginFromRegisterBtn" href="/login">Log in</a></Link>
+                        </p>
+
+                        <button className="registerBtn" onClick={registerUser}>Register</button>
                     </form>
+
+
                 </div>
 
             </div>
