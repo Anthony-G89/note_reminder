d5ace2e78448235e9f2c1706c5b250436870cb13
Anthony Guerrero <adguerrero1935@gmail.com>
2021-09-21 21:17:16 -0400

    Added DB in MYSQL, a Note modal, and server. My server is now connecting to my DB

---
+++ models/note.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, DataTypes) {
+    const Note = sequelize.define("Note", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 20]
+            }
+        }
+
+    });
+
+    return Note;
+}
