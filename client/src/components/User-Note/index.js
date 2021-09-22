5fb7f013e0e3346b949c46617db1f8cdc8b587a0
Anthony Guerrero <adguerrero1935@gmail.com>
2021-09-22 18:17:32 -0400

    Made a navBar in the userDashbored and now going to start working on the body structure

--- client/src/components/User-Note/index.js
+++ client/src/components/User-Note/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import "./style.css";
+import { Link } from 'react-router-dom';
 
 const UserNote = () => {
     return (
 
-        <div className="loginWrapper">
-            <h1>User Notess</h1>
+        <div className="userNoteWrapper">
+            <header className="headerDiv">
+                <Link to="/"><h2 className="appTitleInDashbored">Note Reminder</h2></Link>
+                <nav>
+                    <ul className="logoutContainer">
+                        <li className="logoutBtn">Logout</li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     )
 };
